a428b4e1d0d593741b3f3b5241a4b1fb2ac2ea3e
mohamadp91 <69380537+mohamadp91@users.noreply.github.com>
2023-03-19 15:33:31 +0330

    init the project (#1)

---
+++ src/main/kotlin/com/example/CoffeOrdering/CoffeOrderingApplication.kt
@@ -0,0 +1,11 @@
+package com.example.CoffeOrdering
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CoffeOrderingApplication
+
+fun main(args: Array<String>) {
+	runApplication<CoffeOrderingApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/CoffeOrdering/CoffeOrderingApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.CoffeOrdering
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CoffeOrderingApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
